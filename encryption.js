fe2125f128d9e1f9c2be4742639898aa784e221e
sarnickaa <sarnicka.aleks@gmail.com>
2018-07-14 20:38:46 -0400

    passing all but 3 decryption tests

--- encryption.js
+++ encryption.js
@@ -1,4 +1,3 @@
-
 // For building the encrypted string:
 // Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
 // Do this n times!
@@ -18,27 +17,53 @@
 
 
 let test = "This is a test!"
-arr = test.split("")
+arr = test.split("") // splits string into characters
 
 function encrypt(text, n) {
+  input = text
 
-secondsFilter = function(elem, index, arr) {
-  return (index % 2 !== 0)
-}
-oldFilter = function(elem, index, arr) {
-  return (index % 2 === 0)
-}
 
-for (let i = 0; i < n; i++) {
-arr = text.split("")
-seconds = arr.filter(secondsFilter).join("")
-old = arr.filter(oldFilter).join("")
-}
-resultString = seconds + old
-return resultString
-}
+  secondsFilter = function(elem, index, arr) {
+    return (index % 2 !== 0)
+  }
+  // creates a new array: every odd index (every 2nd elem) pushed into seconds array
 
+  oldFilter = function(elem, index, arr) {
+    return (index % 2 === 0)
+  }
+  // creates a new array: every even index (starting at 0) pushed into olds array
+
+
+  for (let h = 0; h < n; h++) {
+    arr = input.split("")
+    for (let i = 0; i < arr.length; i++) {
+      // arr = input.split("")
+      seconds = arr.filter(secondsFilter).join("")
+      old = arr.filter(oldFilter).join("")
+    }
+    input = seconds + old
+  }
+  return input
+}
 
 function decrypt(encryptedText, n) {
+  input = encryptedText
 
+  secondsFilter = function(elem, index, arr) {
+    return (index % 2 !== 0)
+  }
+  oldFilter = function(elem, index, arr) {
+    return (index % 2 === 0)
+  }
+
+  for (let h = 0; h < n; h++) {
+    arr = input.split("")
+    for (let i = 0; i < arr.length; i++) {
+      // arr = input.split("")
+      seconds = arr.filter(secondsFilter).join("")
+      old = arr.filter(oldFilter).join("")
+    }
+    input = seconds + old
+  }
+  return input
 }
